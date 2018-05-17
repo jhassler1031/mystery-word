5e7b949560f93d4ccdae55c43c615d7dfeab56ca
jhassler1031 <jhassler1031@gmail.com>
2018-05-17 14:18:52 -0400

    committing before big changes

--- mys_word_functions.py
+++ mys_word_functions.py
@@ -4,23 +4,24 @@ def display_state(miss_count, display_word):
     print(display_word)
     return
 
-def mod_display(letter, display_word):
+def mod_display(guess, display_word):
     display_list = list(display_word)
     count = 0
+    return display_word
 
 
-def is_in_word(letter, rand_word, display_word):
+def is_in_word(guess, rand_word, display_word):
     if letter in rand_word:
-        mod_display(letter, display_word)
-        return True
+        display_word = mod_display(guess, display_word)
+        return display_word
     else:
-        return False
+        return display_word
 
 def request_guess(correct_letters, incorrect_letters):
     guess = input("Please guess a letter: ")
     if guess in correct_letters or guess in incorrect_letters:
         print("You have already guessed that letter.")
-        request_guess(correct_letters, incorrect_letters)
+        return request_guess(correct_letters, incorrect_letters)
     else:
         return guess
 
@@ -30,4 +31,13 @@ def request_guess(correct_letters, incorrect_letters):
 def player_turn(rand_word, display_word, correct_letters, incorrect_letters, miss_count):
     display_state(miss_count, display_word)
     guess = request_guess(correct_letters, incorrect_letters)
-    
+    if is_in_word(guess):
+        print("Good guess, that letter is in the word.")
+        correct_letters.append(guess)
+        display_word = mod_display(guess, display_word)
+        return display_word, correct_letters, incorrect_letters, miss_count
+    else:
+        print("Sorry, that letter is not in the word.")
+        incorrect_letters.append(guess)
+        miss_count -= 1
+        return display_word, correct_letters, incorrect_letters, miss_count
