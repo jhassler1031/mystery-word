2a9d3101ab877257883e9e8733a4fe6b8e5ad5a3
jhassler1031 <jhassler1031@gmail.com>
2018-05-17 15:15:23 -0400

    normal mode functions working

--- mys_word_functions.py
+++ mys_word_functions.py
@@ -8,29 +8,23 @@ def display_word(correct_letters, rand_word):
             display_list.append("_")
     return "".join(display_list)
 
-def display_state(miss_count, correct_letters, rand_word):
-    print("You have " + str(miss_count) + " misses remaining.")
-    print(display_word(corrand_letters, rand_word))
-    return
-
 def request_guess(correct_letters, incorrect_letters):
-    guess = input("Please guess a letter: ")
+    guess = input("Please guess a letter: ").lower()
     if guess in correct_letters or guess in incorrect_letters:
         print("You have already guessed that letter.")
         return request_guess(correct_letters, incorrect_letters)
     else:
         return guess
 
-
 def player_turn(rand_word, correct_letters, incorrect_letters, miss_count):
-    print(display_state(miss_count, correct_letters, rand_word))
+    #print(display_state(miss_count, correct_letters, rand_word))
     guess = request_guess(correct_letters, incorrect_letters)
     if guess in rand_word:
         print("Good guess, that letter is in the word.")
         correct_letters.append(guess)
-        return display_word, correct_letters, incorrect_letters, miss_count
+        return correct_letters, incorrect_letters, miss_count
     else:
         print("Sorry, that letter is not in the word.")
         incorrect_letters.append(guess)
         miss_count -= 1
-        return display_word, correct_letters, incorrect_letters, miss_count
+        return correct_letters, incorrect_letters, miss_count
