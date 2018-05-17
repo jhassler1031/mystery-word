1933406730627067c988e3c9bd3fc7959e100680
jhassler1031 <jhassler1031@gmail.com>
2018-05-17 15:47:28 -0400

    added muliple letter check and option to play again

--- mystery-word-hard-mode.py
+++ mystery-word-hard-mode.py
@@ -1,7 +1,7 @@
 
 #connect to functions in separate file
 
-from mys_word_functions import player_turn, display_word
+from mys_word_functions_hard import player_turn, display_word
 #The computer needs to choose a word at random
 
 with open("/usr/share/dict/words") as infile:
@@ -19,22 +19,27 @@ word_length = len(rand_word)
 miss_count = 8
 correct_letters = []
 incorrect_letters = []
+wants_to_play = True
 
 
 #Start user interface
 
 print("Game Start:  The word is " + str(word_length) + " letters long.  Good luck!")
 
-while True:
-    print("You have " + str(miss_count) + " misses remaining.")
-    print(display_word(correct_letters, rand_word))
-    correct_letters, incorrect_letters, miss_count = player_turn(rand_word, correct_letters, incorrect_letters, miss_count)
+while wants_to_play:
+    while True:
+        print("You have " + str(miss_count) + " misses remaining.")
+        print(display_word(correct_letters, rand_word))
+        correct_letters, incorrect_letters, miss_count = player_turn(rand_word, correct_letters, incorrect_letters, miss_count)
 
-    if rand_word == display_word(correct_letters, rand_word):
-        print("Congratulations!  You've won!")
-        break
-    elif miss_count <= 0:
-        print("I'm sorry, you've lost.")
-        break
-    else:
-        print("Next turn.")
+        if rand_word == display_word(correct_letters, rand_word):
+            print("Congratulations!  You've won!")
+            break
+        elif miss_count <= 0:
+            print("I'm sorry, you've lost.")
+            break
+        else:
+            print("Next turn.")
+    answer = input("Would you like to play again? Y or N: ")
+    if answer.lower() != "y":
+        wants_to_play = False
